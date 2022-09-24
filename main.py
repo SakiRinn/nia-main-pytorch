1408f5376281cc3fa46b1a648191bb6103e1ee07
SakiRinn <sakirinn@qq.com>
2022-09-24 18:17:41 +0800

    Organizing file structure.

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import numpy as np
 import datasets
 import datasets.tools as tools
 import modules
+from modules.seq2seq import AttentionSeq2Seq
 
 train = True
 test = True
 
-seq2seq = None
-
 
 def predict(username, origin, destination, targets, middleboxes, qos, start, end, allow, block):
     global seq2seq
-    entities = tools.anonymize(username, origin, destination, targets, middleboxes, qos, start, end, allow, block)
-    intent = seq2seq.predict(entities)
+    entities = tools.anonymize(username, origin, destination, targets,
+                               middleboxes, qos, start, end, allow, block)
+    intent = AttentionSeq2Seq.predict(entities)
     print('intent', intent)
-    result = tools.deanonymize(deanonymize, username, origin, destination, targets, middleboxes, qos, start, end, allow, block)
+    result = tools.deanonymize(intent, username, origin, destination, targets,
+                               middleboxes, qos, start, end, allow, block)
     print('result', result)
 
     return result
@@ -23,7 +24,7 @@ def predict(username, origin, destination, targets, middleboxes, qos, start, end
 
 def main():
     global seq2seq, train, test
-    input_words, output_words = datasets.read()
+    input_words, output_words = tools.read()
 
     # Creating the network model
     seq2seq = modules.AttentionSeq2Seq(input_words, output_words)
