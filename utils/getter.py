c01759c99c853d01f8f948d08690e87448857982
SakiRinn <sakirinn@qq.com>
2022-10-05 02:48:34 +0800

    edit sequence of getter.

--- utils/getter.py
+++ utils/getter.py
@@ -13,16 +13,16 @@ def get_loss(name: str, *args, **kwargs):
     import torch.nn
     import modules.loss
 
-    if hasattr(torch.nn, name):
-        try:
-            return getattr(torch.nn, name)(*args, **kwargs)
-        except TypeError:
-            return getattr(torch.nn, name)()
-    elif hasattr(modules.loss, name):
+    if hasattr(modules.loss, name):
         try:
             return getattr(modules.loss, name)(*args, **kwargs)
         except TypeError:
             return getattr(modules.loss, name)()
+    elif hasattr(torch.nn, name):
+        try:
+            return getattr(torch.nn, name)(*args, **kwargs)
+        except TypeError:
+            return getattr(torch.nn, name)()
     raise AttributeError(f'There is no class named {name}.')
 
 
@@ -41,16 +41,16 @@ def get_optimizer(name: str, params, lr, *args, **kwargs):
     import torch.optim
     import modules.optimizer
 
-    if hasattr(torch.optim, name):
-        try:
-            return getattr(torch.optim, name)(params, lr, *args, **kwargs)
-        except TypeError:
-            return getattr(torch.optim, name)(params, lr)
-    elif hasattr(modules.optimizer, name):
+    if hasattr(modules.optimizer, name):
         try:
             return getattr(modules.optimizer, name)(params, lr, *args, **kwargs)
         except TypeError:
             return getattr(modules.optimizer, name)(params, lr)
+    elif hasattr(torch.optim, name):
+        try:
+            return getattr(torch.optim, name)(params, lr, *args, **kwargs)
+        except TypeError:
+            return getattr(torch.optim, name)(params, lr)
     raise AttributeError(f'There is no class named {name}.')
 
 
